ba7ec83417b6c1fabd4ced35f640a34a10970fa7
nazaninsmac <nzn.amabilis@gmail.com>
2025-06-04 13:58:12 +0330

    changed to cammelcase

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 # for avoiding hardcoding some paths vars and stuff
+
+
+SR_MODEL_PATH = "image_processing/models/EDSR_x4.pb"
+SHAPE_PREDICTOR_PATH = "image_processing/models/shape_predictor_68_face_landmarks.dat" 
